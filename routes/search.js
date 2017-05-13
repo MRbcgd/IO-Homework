92fc7a6ae4e68c652eb809f045d40d4c91c03e8c
MRbcgd <quittypig@gmail.com>
2017-05-13 20:15:24 +0900

    search UI Build, data search button test

--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {//라우터 연동 테스트
-  res.render('search', { title: 'test of search' });
+  res.render('search');
+});
+
+router.post('/', function(req, res, next){
+  console.log(req.body);//test of search data
 });
 
 module.exports = router;
