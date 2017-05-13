8bd20b1b5f9c6ac9e9cbd682d22122b2bc4ef6c7
MRbcgd <quittypig@gmail.com>
2017-05-13 21:26:57 +0900

    [test code]find json data, use API, blockchain & request module

--- routes/search.js
+++ routes/search.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var blockexplorer = require('blockchain.info/blockexplorer');//blockchain module
+var request = require('request');//find json data, use url
 
 /* GET home page. */
 router.get('/', function(req, res, next) {//라우터 연동 테스트
@@ -7,7 +10,23 @@ router.get('/', function(req, res, next) {//라우터 연동 테스트
 });
 
 router.post('/', function(req, res, next){
-  console.log(req.body);//test of search data
+  var search_blockhash = req.body.search_blockhash;
+  var inoutput = req.body.inoutput;
+
+  console.log('search_blockhash : ',search_blockhash);
+  console.log('inoutput : ',inoutput);
+
+  //find json data, use url
+  request('https://blockchain.info/ko/rawblock/'+search_blockhash, function (err, response, body) {
+    if (!err && response.statusCode == 200) {
+       var importedJSON = JSON.parse(body);
+
+       res.redirect('/search/argument1='+search_blockhash+'&argument2='+inoutput);
+    } else {//Wrong Block Hash
+      console.log('ERROR : Wrong Block Hash!');
+      res.redirect('/search');
+    }
+  });
 });
 
 module.exports = router;
