7d5190b73cdb751ccd1f8ec60eb0fbbf2e5cae4e
MRbcgd <quittypig@gmail.com>
2017-05-14 03:35:36 +0900

    [test code] input, ouput에 따른 transaction inputs, out 결과 확인

--- routes/search.js
+++ routes/search.js
@@ -17,12 +17,14 @@ router.get('/', function(req, res, next) {//라우터 연동 테스트
 
     request('https://blockchain.info/ko/rawblock/'+search_blockhash, function (err, response, body) {
       if ( !err && response.statusCode == 200 ) {
-
         var importedJSON = JSON.parse(body);
         var sum_size = 0; //block의 tx size와 실제 size 총 합 비교를 위한 변수
         var sum_value = 0; //input,output의 총 value 합
         var avg_value = 0; //value 평균
-        var count = 0; //총 input, output 개수
+        var count_value = 0; //총 input, output  value 개수
+        var avg_fee = 0;
+        var avg_size = 0;
+        var inout_results = [];//input or ouput의 결과 출력을 위한 변수
 
         //SIZE TEST
         for( var j = 0; j < importedJSON.tx.length; j++ ) {
@@ -39,7 +41,7 @@ router.get('/', function(req, res, next) {//라우터 연동 테스트
           //input
           for (var inputs_length = 0; inputs_length < importedJSON.tx[i].inputs.length; inputs_length++) {
             if(importedJSON.tx[i].inputs[inputs_length].prev_out !== undefined ){
-              count++;
+              count_value++;
               console.log('inputs value : ',importedJSON.tx[i].inputs[inputs_length].prev_out.value);
               sum_value += importedJSON.tx[i].inputs[inputs_length].prev_out.value;
             } else {
@@ -49,16 +51,44 @@ router.get('/', function(req, res, next) {//라우터 연동 테스트
           //output
           for (var output_length = 0; output_length < importedJSON.tx[i].out.length; output_length++) {
             if(importedJSON.tx[i].out[output_length] !== undefined ){
-              count++;
+              count_value++;
               console.log('out value : ',importedJSON.tx[i].out[output_length].value);
               sum_value += importedJSON.tx[i].out[output_length].value;
             } else {
               console.log('out value : empty data');
             }
           }
-          console.log('sum value : ',sum_value);//input, output의 총 수량
-          console.log('count : ',count);//총 수량
+          console.log('sum value : ',sum_value);//input, output의 value 총 합계
+          console.log('count_value : ',count_value);//input, output의 value 총 개수
         }
+
+        //inout에 따른 요구사항2 결과 출력
+        if( req.query.inoutput === 'input' ) {//input
+          for(var inputs_results = 0; inputs_results < importedJSON.tx.length; inputs_results++){
+            for (var inputs_results_array = 0; inputs_results_array < importedJSON.tx[inputs_results].inputs.length; inputs_results_array++) {
+              inout_results.push(JSON.stringify(importedJSON.tx[inputs_results].inputs[inputs_results_array]));
+            }
+          }
+        } else{//output
+          for(var out_results = 0; out_results < importedJSON.tx.length; out_results++){
+            for (var out_results_array = 0; out_results_array < importedJSON.tx[out_results].out.length; out_results_array++) {
+              inout_results.push(JSON.stringify(importedJSON.tx[out_results].out[out_results_array]));
+            }
+          }
+        }
+
+        avg_value = sum_value / count_value;//평균 value
+        avg_fee = importedJSON.fee / importedJSON.n_tx;//평균 fee
+        avg_size = importedJSON.size / importedJSON.n_tx;//평균 size
+
+        res.render('search',{
+          blockhash : search_blockhash,
+          prev_block : importedJSON.prev_block,
+          n_tx : importedJSON.n_tx,
+          avg_value : avg_value,
+          avg_fee : avg_fee,
+          avg_size : avg_size
+        });
       } else {//Wrong Block Hash
         console.log('ERROR : Wrong Block Hash!');
         res.redirect('/search');
